8b6ec9327b009c75157ff19204359ace83d859fa
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2015-06-06 14:52:56 -0400

    Cleaned up code. Made a little responsive.

--- js/site.js
+++ js/site.js
@@ -1,16 +1,16 @@
 //List of CrossFit movements
-  var wodArray = ["<a href=index.html>Home</a>", "<a href=history.html>History</a>", "<a href=clean.html>Clean & Jerk</a>", "<a href=snatch.html>Snatch</a>", "<a href=muscleup.html>Muscle Up</a>", "<a href=hspu.html>HSPU</a>"];
+var wodArray = ["<a href=index.html>Home</a>", "<a href=history.html>History</a>", "<a href=clean.html>Clean & Jerk</a>", "<a href=snatch.html>Snatch</a>", "<a href=muscleup.html>Muscle Up</a>", "<a href=hspu.html>HSPU</a>"];
   
 //param {wodData} array of wods
-  var wodTemplete = function(wodData) {
-        var markup = "";
-        var i; 
+var wodTemplete = function(wodData) {
+      var markup = "";
+      var i; 
 
-        for(i = 0; i < wodData.length; i++) {//i++ adds one
-          markup += "<li>" + wodData[i] + "</li>";
-        }
-        return markup; 
-  };
+      for(i = 0; i < wodData.length; i++) {//i++ adds one
+        markup += "<li>" + wodData[i] + "</li>";
+      }
+      return markup; 
+};
 
 $(".wod").append(wodTemplete(wodArray));
 //end of crossfit movements
@@ -18,26 +18,26 @@ $(".wod").append(wodTemplete(wodArray));
 
 
 //photo album for home page
-  var albumArray = ["<img src=images/kferris.jpg>", "<img src=images/klokov.jpg>", "<img src=images/muscleup.png>", "<img src=images/hspu.jpg>"];
+var albumArray = ["<img src=images/kferris.jpg>", "<img src=images/klokov.jpg>", "<img src=images/muscleup.png>", "<img src=images/hspu.jpg>"];
 
-    var albumTemplete = function(albumData) {
-        var markup = "";
-        var i; 
+var albumTemplete = function(albumData) {
+      var markup = "";
+      var i; 
 
-        for(i = 0; i < albumData.length; i++) {//i++ adds one
-          markup += "<div>" + albumData[i] + "</div>";
-        }
-        return markup; 
-  };
+      for(i = 0; i < albumData.length; i++) {//i++ adds one
+        markup += "<div>" + albumData[i] + "</div>";
+      }
+      return markup; 
+};
 
 $(".album").append(albumTemplete(albumArray));
 
-  //To Show and Hide pictures
+//To Show and Hide pictures
 $(".album img").click(function(event) {
-      event.preventDefault();
+        event.preventDefault();
 
-$(this).closest("div").siblings().toggle();
-$(this).toggleClass("magnify");
+  $(this).closest("div").siblings().toggle();
+  $(this).toggleClass("magnify");
 });
 //end of photo album
 
@@ -47,27 +47,27 @@ $(this).toggleClass("magnify");
 //Table of contents using jQuery.each() 
 $(document).ready(function() {
 
-var contents = [
-  {
-    section: "Glassman"
-  },
-  {
-    section: "Affiliates"
-  },
-  {
-    section: "Crossfit Games"
-  }
-];
+  var contents = [
+    {
+      section: "Glassman"
+    },
+    {
+      section: "Affiliates"
+    },
+    {
+      section: "Crossfit Games"
+    }
+  ];
 
-var markup = "";
+  var markup = "";
 
-  $.each(contents, function(key, value) {
+    $.each(contents, function(key, value) {
 
-    markup += "<ul>" + contents[key].section + "</ul>";
+      markup += "<ul>" + contents[key].section + "</ul>";
 
-  }); 
+    }); 
 
-$(".contents").append(markup);
+  $(".contents").append(markup);
 
 });
 
